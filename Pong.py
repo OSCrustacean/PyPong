f0acb3fdac9a751dfefaa9036b200df9a8fb6e6e
OSCrustacean <OSCrustacean@users.noreply.github.com>
2020-05-22 18:58:51 -0500

    Better scalability and reset screen

--- Pong.py
+++ Pong.py
@@ -21,11 +21,14 @@ score_b = 0
 place_x=half_width-(width/16) ##for width 800; 350
 place_y=half_height-(height/10) ##for height 600; 240
 
+width_stretch_factor = height/120
+len_stretch_factor = width/800
+
 paddle_a = turtle.Turtle() #First Paddle
 paddle_a.speed(0)
 paddle_a.shape("square")
 paddle_a.color("white")
-paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+paddle_a.shapesize(stretch_wid=width_stretch_factor, stretch_len=len_stretch_factor)
 paddle_a.penup()
 paddle_a.goto(-place_x, 0)
 
@@ -33,18 +36,21 @@ paddle_b = turtle.Turtle() #Second Paddle
 paddle_b.speed(0)
 paddle_b.shape("square")
 paddle_b.color("white")
-paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+paddle_b.shapesize(stretch_wid=width_stretch_factor, stretch_len=len_stretch_factor)
 paddle_b.penup()
 paddle_b.goto(place_x, 0)
+
+default_speed = half_width/65
 
 ball = turtle.Turtle() #Ball
 ball.speed(0)
 ball.shape("square")
 ball.color("white")
 ball.penup()
+ball.shapesize(stretch_wid=width/800, stretch_len=height/600)
 ball.goto(0 , 0)
-ball.dx = 6 #Speed
-ball.dy = 2
+ball.dx = default_speed #Speed
+ball.dy = half_height/150 #default 2
 
 pen = turtle.Turtle() #Pen
 pen.speed(0)
@@ -55,7 +61,7 @@ pen.goto(0, place_y)
 pen.write("Player A: " + str(score_a) + "  " + "Player B: " + str(score_b), align="center", font=("Courier", 18, "normal"))
 
 #Functions
-movement=35
+movement=half_height/8 #default ~35
 
 def paddle_a_up():
     y = paddle_a.ycor()
@@ -88,33 +94,41 @@ wn.onkeypress(paddle_b_down, "Down")
 y_edge=half_height-(height/60) #default height 600;290
 x_edge=half_width-(width/80) #default width 800; 390
 
-buffer_value=50 #default 50 #half_height/6
-upper_value=place_x #default 350 #half_height + 50
-lower_value=upper_value-10 #default 340
+buffer_value=half_height/6 #default 50 #half_height/6
+upper_value=half_height + buffer_value #default 350 #half_height + 50
+lower_value=upper_value - half_height/20 #default 340
 
 
 def paddle_a_bounce():
     if (ball.xcor() < -lower_value and ball.xcor() > -upper_value) and (ball.ycor() < paddle_a.ycor() + buffer_value and ball.ycor() > paddle_a.ycor() -buffer_value):
         ball.setx(-lower_value)
+        ball.dx -= 1
         ball.dx *= -1
-    if paddle_a.ycor() > half_height - buffer_value: #keep paddle_a from going off edge
-        paddle_a.sety(half_height - 50)
-    if paddle_a.ycor() < -half_height + buffer_value: #keep paddle_a from going off edge
-        paddle_a.sety(-half_height + 50)
+    if paddle_a.ycor() > half_height - buffer_value: #keep paddle_a from going off top edge
+        paddle_a.sety(half_height - buffer_value)
+    if paddle_a.ycor() < -half_height + buffer_value: #keep paddle_a from going off bottom edge
+        paddle_a.sety(-half_height + buffer_value)
 
 def paddle_b_bounce():
     if (ball.xcor() > lower_value and ball.xcor() < upper_value) and (ball.ycor() < paddle_b.ycor() + buffer_value and ball.ycor() > paddle_b.ycor() -buffer_value):
         ball.setx(lower_value)
+        ball.dx += 1
         ball.dx *= -1
-    if paddle_b.ycor() > half_height - buffer_value: #keep paddle_b from going off edge
-        paddle_b.sety(half_height - 50)
-    if paddle_b.ycor() < -half_height + buffer_value: #keep paddle_b from going off edge
-        paddle_b.sety(-half_height + 50)
+    if paddle_b.ycor() > half_height - buffer_value: #keep paddle_b from going off top edge
+        paddle_b.sety(half_height - buffer_value)
+    if paddle_b.ycor() < -half_height + buffer_value: #keep paddle_b from going off bottom edge
+        paddle_b.sety(-half_height + buffer_value)
 
 def paddle_functions():
     paddle_a_bounce()
     paddle_b_bounce()
 
+def reset_position():
+    ball.goto(0, 0)
+    paddle_a.sety(0)
+    paddle_b.sety(0)
+    ball.dx = default_speed
+    time.sleep(1)
 
 
 #Main loop
@@ -134,23 +148,24 @@ while True:
         ball.dy *= -1
 
     if ball.xcor() > x_edge: #Right side; w/ width 800 = 390
-        ball.goto(0, 0)
         ball.dx *= -1
         score_a += 1
         pen.clear()
         pen.write("Player A: " + str(score_a) + "  " + "Player B: " + str(score_b), align="center", font=("Courier", 18, "normal"))
+        reset_position()
+
 
     if ball.xcor() < -x_edge: #Left side
-        ball.goto(0, 0)
         ball.dx *= -1
         score_b += 1
         pen.clear()
         pen.write("Player A: " + str(score_a) + "  " + "Player B: " + str(score_b), align="center", font=("Courier", 18, "normal"))
+        reset_position()
 
     #Paddles
     paddle_functions()
 
-    #Score
+    #End game
     if score_a > 4:
         paddle_a.goto(-place_x, 0)
         paddle_b.goto(place_x, 0)
